b48e414ad9d1d947345d681f433a485c102194a3
ChenZhaoYu <790348264@qq.com>
2023-02-01 09:22:59 +0800

    chore: taobao mirror sync

--- scripts/sync.ts
+++ scripts/sync.ts
@@ -1,3 +1,13 @@
-async function sync() {}
+import { fetch } from 'zx';
+
+const requestUrl = 'https://npmmirror.com/sync/eslint-config-redon';
+
+const pkgs = ['base', 'ts', '', 'vue', 'react', 'react-native', 'svelte', 'solid'];
+
+const requestUrls = pkgs.map(item => requestUrl + (item ? `-${item}` : ''));
+
+async function sync() {
+  await Promise.all(requestUrls.map(url => fetch(url)));
+}
 
 sync();
